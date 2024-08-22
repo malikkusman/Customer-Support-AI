45d542ee45a89bd2d2979dda9aceac1ca8432e80
malikkusman <42khan0@gmail.com>
2024-08-22 17:39:18 +0500

    Update Server

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,24 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const morgan = require("morgan");
 const { connectDB } = require("./utils/db");
+const userroutes = require("./routes/userRoutes");
+const chatroutes = require("./routes/chatRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
+app.use(express.json());
 app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+
+// Routes
+app.use("/api", userroutes);
+app.use("/api", chatroutes);
 
 // Connect to MongoDB and start the server
 connectDB().then(() => {
